cce4f38b7dad102cb10d9e748549a16923a8da47
glunfront <glunfront@gmail.com>
2022-02-10 13:50:10 +0500

    udp_ScrollTop-comp

--- src/components/ScrollTop/ScrollTop.jsx
+++ src/components/ScrollTop/ScrollTop.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useActions } from '../../hooks/useActions';
 
 const ScrollTop = ({children}) => {
     const location = useLocation()
+    const {changePartnerAmountAction} = useActions()
 
     useEffect(() => {
+        changePartnerAmountAction(0)
         window.scrollTo(0, 0)
     },[location])
 
